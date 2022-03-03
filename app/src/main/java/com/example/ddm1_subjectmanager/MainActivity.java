36edfd3cb3d459e81ccad3017fb7d98b72d51ce7
faozibouybaouenegadrouz <fbg171@gmail.com>
2022-03-03 19:01:32 +0100

    Showed first list with names of subjects.

--- app/src/main/java/com/example/ddm1_subjectmanager/MainActivity.java
+++ app/src/main/java/com/example/ddm1_subjectmanager/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.ddm1_subjectmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<Subject> subjects = new ArrayList<>();
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
         subjects.add(new Subject("ASO", R.drawable.asologo, "Administració de Sistemes Operatius"));
         subjects.add(new Subject("PiC", R.drawable.piclogo, "Pensament i Creativitat"));
+
+        String names[] = new String[subjects.size()];
+
+        for (int i = 0; i < subjects.size(); i++) {
+            names[i] = subjects.get(i).getTitle();
+        }
+
+        list = findViewById(R.id.listView);
+
+        ArrayAdapter<String> array = new ArrayAdapter<String>(this,R.layout.activity_listview, R.id.subjectTitle,names);
+
+        list.setAdapter(array);
+
+
+
+
     }
 }
